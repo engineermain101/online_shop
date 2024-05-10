c860a4f948ffd9a6928657d060d6a33f32326816
PuiaVlad26 <puiavlad95@gmail.com>
2024-05-10 19:12:32 +0300

    tes
    te

--- shop_online/shop_online/Test.cs
+++ shop_online/shop_online/Test.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Drawing;
 using Xunit;
+using shop_online;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace ShopOnline.Tests
 {
@@ -26,15 +28,70 @@ namespace ShopOnline.Tests
             Image image = Image.FromFile("path_to_image/image.jpg");
 
             // Apelarea metodei pentru a insera imaginea
-            bool insertionResult = shopOnline.InsertImage(connectionString, image, productId, imageName);
+            // bool insertionResult = Interogari.InsertImagine(connectionString, image, productId, imageName);
 
             // Verificarea dacă inserarea a fost efectuată cu succes
-            Assert.True(insertionResult, "Image insertion failed.");
+            // Assert.True(insertionResult, "Image insertion failed.");
         }
 
-        private bool InsertImage(string connectionString, Image image, int productId, string imageName)
+        [Fact]
+        public void SignUp_All_Parameters_Null()
+        {//pt singup
+         //Arrange connection string 
+            string connectionString = null;
+            string nume = null;
+            string email = null;
+            string parola = null;
+            string telefon = null;
+            string judet = null;
+            string oras = null;
+            string strada = null;
+            int numar = 0;
+            //Act  
+            bool value = Interogari.SignUp(connectionString, nume, email, parola, telefon, judet, oras, strada, numar);
+            //Asert  
+            Assert.Equal(false, value);
+        }
+        [Fact]
+        public void SignUp_All_Parameters_fill()
+        {//pt singup
+         //Arrange connection string 
+            string connectionString = null;
+            string nume = "roly";
+            string email = "roly593593.com";
+            string parola = "12.,.l";
+            string telefon ="09jgjdf";
+            string judet = "323432324";
+            string oras = "324";
+            string strada = "*^$^*";
+            int numar = 2;
+            //Act  
+            bool value = Interogari.SignUp(connectionString, nume, email, parola, telefon, judet, oras, strada, numar);
+            //Asert  
+            Assert.Equal(false, value);
+        }
+        [Fact]
+        public void Login_Null()
         {
-            throw new NotImplementedException();
+            string connectionString = null;
+            string email = null;
+            string parola = null;
+            string telefon = null;
+            bool value = Interogari.Login(connectionString, email, parola, telefon);
+            Assert.Equal(false, value);
+
+        }
+        public void Login_fill()
+        {
+            string connectionString = null;
+            string email = "32435claus";
+            string parola = "";
+            string telefon = "fdsgdf";
+            bool value = Interogari.Login(connectionString, email, parola, telefon);
+            Assert.Equal(false, value);
+
         }
     }
+    
+    
 }
